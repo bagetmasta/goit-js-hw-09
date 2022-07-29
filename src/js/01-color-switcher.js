a05e4808abad30b3f6d24921bb281084fe43451b
Artem Bahmet <baget.masta@gmail.com>
2022-07-29 05:49:43 +0300

    save 1st resolved task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+refs.stopButton.setAttribute('disabled', true);
+
+function onStartButtonClick() {
+  refs.startButton.setAttribute('disabled', true);
+  refs.stopButton.removeAttribute('disabled', true);
+
+  chahgeBgColor();
+
+  timerId = setInterval(() => {
+    chahgeBgColor();
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  refs.startButton.removeAttribute('disabled', false);
+  refs.stopButton.setAttribute('disabled', true);
+  clearInterval(timerId);
+}
+
+function chahgeBgColor() {
+  const bodyColor = getRandomHexColor();
+  refs.body.style.backgroundColor = bodyColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+// Описан в документации
+const flatpickr = require('flatpickr');
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  inputText: document.querySelector('[datetime-picker]'),
+};
+
+console.log(refs.inputText);
